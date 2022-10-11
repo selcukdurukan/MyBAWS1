9c9e3ca4b7a7d3efcd8a96f480d32d7723d5cfd6
selcukdurukan <selcukdurukan@outlook.com.tr>
2022-10-11 12:07:00 +0300

    11-11-2022

---
+++ src/com/ba/boost/atclass/AdditionWithoutAddition.java
@@ -0,0 +1,31 @@
+package com.ba.boost.atclass;
+
+public class AdditionWithoutAddition {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 15;
+		
+		System.out.println(AdditionWithoutAddition.add(a,b));
+
+	}
+
+	private static int add(int a, int b) {
+		if (a == 0) return b;
+		if (b == 0) return a;
+		if (a > 0) return a= -a;
+		if (b < 0) b = -b;
+		
+		if (b > 0) {
+			for (int i = b; i > 0; i--) {
+				a -=1;
+			}
+		} else {
+
+		}
+		
+		return -a;
+	}
+
+}
